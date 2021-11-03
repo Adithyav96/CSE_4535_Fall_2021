a5d243bbbb64336cd48e38e253875dfac017babf
Adithya <adi5ver@gmail.com>
2021-11-03 02:06:20 -0400

    Project3 phase 1 check

--- project3/project3_data/json_to_trec.py
+++ project3/project3_data/json_to_trec.py
@@ -3,22 +3,35 @@
 
 import json
 # if you are using python 3, you should 
-#import urllib.request 
-import urllib2
+import urllib.request 
+from urllib.parse import quote
+#import urllib2
 
 
 # change the url according to your own corename and query
-inurl = 'http://localhost:8983/solr/corename/select?q=*%3A*&fl=id%2Cscore&wt=json&indent=true&rows=20'
-outfn = 'path_to_your_file.txt'
+#inurl = 'http://localhost:8983/solr/corename/select?q=*%3A*&fl=id%2Cscore&wt=json&indent=true&rows=20'
+core1 = ['VSM_1','BM25_1']
+input1 = ['Anti-Refugee Rally in Dresden','Syrian civil war','Assad und ISIS auf dem Vormarsch','Russische Botschaft in Syrien von Granaten getroffen','Бильд. Внутренний документ говорит, что Германия примет 1,5 млн беженцев в этом году']
+ip = '18.219.42.216'
+
+for core in core1:
+    encoded_input = quote(input)
+    print(encoded_input)
+
+    inurl = f'http://{ip}:8983/solr/{core}/select?q=text_en:{encoded_input}%20OR%20text_de:{encoded_input}%20OR%20text_ru:{encoded_input}&fl=id%2Cscore&wt=json&indent=true&rows=20'
+    outfn = 'q3.txt'
+input = 'Anti-Refugee Rally in Dresden'
+encoded_input = quote(input)
+print(inurl)
 
 
 # change query id and IRModel name accordingly
-qid = ''
-IRModel='' #either bm25 or vsm
+qid = '001'
+IRModel='vsm' #either bm25 or vsm
 outf = open(outfn, 'a+')
-data = urllib2.urlopen(inurl)
+#data = urllib2.urlopen(inurl)
 # if you're using python 3, you should use
-# data = urllib.request.urlopen(inurl)
+data = urllib.request.urlopen(inurl)
 
 docs = json.load(data)['response']['docs']
 # the ranking should start from 1 and increase
---
+++ project3/project3_data/json_to_trec2.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+
+
+import json
+# if you are using python 3, you should 
+import urllib.request 
+from urllib.parse import quote
+
+
+# change the url according to your own corename and query
+#inurl = 'http://localhost:8983/solr/corename/select?q=*%3A*&fl=id%2Cscore&wt=json&indent=true&rows=20'
+core = 'VSM_1'
+ip = '18.219.42.216'
+file1 = open('queries.txt', 'r',encoding='utf-8')
+Lines = file1.readlines()
+file1.close()
+inputlines = []
+for line in Lines:
+    #text = line.split(' ')
+    text = ''
+    for i in range(4,len(line)):
+        text = text + line[i]
+    inputlines.append(text)
+
+
+count = 0
+#input1 = ['Anti-Refugee Rally in Dresden','Syrian civil war','Assad und ISIS auf dem Vormarsch','Russische Botschaft in Syrien von Granaten getroffen','Бильд. Внутренний документ говорит, что Германия примет 1,5 млн беженцев в этом году']
+for line in inputlines:
+    count = count + 1
+    encoded_input = quote(line)
+    print(line)
+    inurl = f'http://{ip}:8983/solr/{core}/select?q=text_en:{encoded_input}%20OR%20text_de:{encoded_input}%20OR%20text_ru:{encoded_input}&fl=id%2Cscore&wt=json&indent=true&rows=20'
+    print(inurl)
+    outfn = 'q2.txt'
+    if count > 9:
+        qid = '0' + str(count)
+    else:
+        qid = '00' + str(count)
+
+    IRModel='vsm' #either bm25 or vsm
+    outf = open(outfn, 'a+')
+    data = urllib.request.urlopen(inurl)
+
+    docs = json.load(data)['response']['docs']
+    rank = 1
+    for doc in docs:
+        outf.write(qid + ' ' + 'Q0' + ' ' + str(doc['id']) + ' ' + str(rank) + ' ' + str(doc['score']) + ' ' + IRModel + '\n')
+        rank += 1
+    outf.close()
